8d9ea6b78f3eb436d5b62484a79e22fb1ea542fb
Alok <alok.shankar.m@gmail.com>
2016-04-03 20:43:28 +0530

    Added effects activity

--- app/src/main/java/com/hack/roposohack/EffectsActivity.java
+++ app/src/main/java/com/hack/roposohack/EffectsActivity.java
@@ -1,15 +1,27 @@
 package com.hack.roposohack;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class EffectsActivity extends AppCompatActivity {
+    private File audioFile;
+    private File videoFile;
+
+    private FloatingActionButton fab;
+    private Button addEffectsButton;
+
+    private boolean effectsSelected;
+    private ArrayList seletedItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +30,48 @@ public class EffectsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        audioFile = new File(getIntent().getStringExtra("audioFile"));
+        videoFile = new File(getIntent().getStringExtra("videoFile"));
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.hide();
+
+        addEffectsButton = (Button) findViewById(R.id.addEffects);
+        addEffectsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                final CharSequence[] items = {" Easy "," Medium "," Hard "," Very Hard "};
+                seletedItems = new ArrayList();
+
+                AlertDialog dialog = new AlertDialog.Builder(EffectsActivity.this)
+                    .setTitle("Select Effects")
+                    .setMultiChoiceItems(items, null, new DialogInterface.OnMultiChoiceClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int indexSelected, boolean isChecked) {
+                            if (isChecked) {
+                                seletedItems.add(indexSelected);
+                            } else if (seletedItems.contains(indexSelected)) {
+                                seletedItems.remove(Integer.valueOf(indexSelected));
+                            }
+                        }
+                    }).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            effectsSelected = true;
+
+                        }
+                    }).create();
+                dialog.show();
             }
         });
 
-        File audioFile = new File(getIntent().getStringExtra("audioFile"));
-        File videoFile = new File(getIntent().getStringExtra("videoFile"));
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Write code to do stuff here
+                }
+            });
+        }
     }
-
 }
